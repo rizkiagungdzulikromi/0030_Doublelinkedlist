f4bd5142e440edfefee8f98e3c40c26f11e5f0a7
rizkiagungdzulikromi <riskiagung3005@gmail.com>
2024-05-04 23:27:58 +0700

    void addnode

--- 0030_Doublelinkedlist.cpp
+++ 0030_Doublelinkedlist.cpp
@@ -12,4 +12,57 @@ struct Node
 
 Node* START = NULL;
 
+void addnode()
+{
+    Node* newNode = new Node();
+    cout << "\nEnter the roll number of the student: ";
+    cin >> newNode->noMhs;
+    cout << "\nEnter the name of the student: ";
+    cin >> newNode->name;
+
+    if (START == NULL || newNode->noMhs <= START->noMhs)
+    {
+        if (START != NULL && newNode->noMhs == START->noMhs)
+        {
+            cout << "\033[31mDuplicate roll number not allowed\033[0m" << endl;
+            return;
+        }
+        newNode->next = START;
+        if (START != NULL)
+        {
+            START->prev = newNode;
+        }
+        newNode->prev = NULL;
+        START = newNode;
+    }
+    else
+    {
+        Node* current = START;
+        Node* previous = NULL;
+
+        while (current != NULL && current->noMhs < newNode->noMhs)
+        {
+            previous = current;
+            current = current->next;
+        }
+
+        newNode->next = current;
+        newNode->prev = previous;
+
+        if (current != NULL)
+        {
+            current->prev = newNode;
+        }
+
+        if (previous != NULL)
+        {
+            previous->next = newNode;
+        }
+        else
+        {
+            START = newNode;
+        }
+    }
+}
+
 
