da12da6b87157800026cca8560e8b1d5b766cd43
rizkiagungdzulikromi <riskiagung3005@gmail.com>
2024-05-04 23:35:56 +0700

    void deleteNode

--- 0030_Doublelinkedlist.cpp
+++ 0030_Doublelinkedlist.cpp
@@ -77,3 +77,54 @@ bool search(int rollNo, Node** previous, Node** current)
     return (*current != NULL);
 }
 
+void deleteNode()
+{
+    Node* previous, * current;
+    int rollNo;
+
+    cout << "\nEnter the roll number of the student whose record is to be deleted: ";
+    cin >> rollNo;
+
+    if (START == NULL)
+    {
+        cout << "List is empty" << endl;
+        return;
+    }
+
+    current = START;
+    previous = NULL;
+
+    while (current != NULL && current->noMhs != rollNo)
+    {
+        previous = current;
+        current = current->next;
+    }
+
+    if (current == NULL)
+    {
+        cout << "\033[31mThe record with roll number " << rollNo << " not found\033[0m" << endl;
+        return;
+    }
+
+    if (current == START)
+    {
+        START = START->next;
+        if (START != NULL)
+        {
+            START->prev = NULL;
+        }
+    }
+    else
+    {
+        previous->next = current->next;
+        if (current->next != NULL)
+        {
+            current->next->prev = previous;
+        }
+    }
+
+    delete current;
+    cout << "\x1b[32mRecord with roll number " << rollNo << " deleted\x1b[0m" << endl;
+}
+
+
